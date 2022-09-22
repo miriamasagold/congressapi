bdc1b1980b106bef2582d5a4515ab1207d090827
miriamgold <goldasa99@gmail.com>
2022-09-21 21:22:02 -0500

    combine R scripts

--- R/congressGet.R
+++ R/congressGet.R
@@ -1,3 +1,140 @@
+congress_make_url <- function(endpoint) {
+
+  base_url <- "https://api.congress.gov/v3"
+
+  full_url <- paste(base_url, endpoint, sep = "/")
+
+  return(full_url)
+
+}
+
+appendArgs <- function(base_list, ...) {
+
+  args_to_add <- list(...)
+
+  starting_length <- length(base_list)
+
+  new_names <- c(names(base_list), names(args_to_add))
+
+  for (i in seq_along(args_to_add)) {
+
+    base_list[[i+starting_length]] <- args_to_add[[i]]
+
+  }
+
+  names(base_list) <- new_names
+
+  return(base_list)
+
+}
+
+
+#===============================================================================
+# Parse API response
+#===============================================================================
+
+extract_endpoint <- function(url) {
+
+  pattern <- "(?<=v3/)[[:alpha:]-]*(?=/?)"
+  m <- regexpr(pattern, url,
+               perl = TRUE)
+
+  endpoint <- regmatches(url, m)
+
+  cleaned <- gsub("-", "_", endpoint)
+
+  return(cleaned)
+
+}
+
+congressParse <- function(resp) {
+
+  content <- httr::content(resp,
+                           as = "text",
+                           encoding = "UTF-8")
+
+  validate <-
+    jsonlite::validate(content)[1]
+
+  if (!validate) {
+
+    stop("API response does not contain valid JSON.\nYour API call was: ",
+         resp$url,
+         call. = FALSE)
+
+  } else {
+
+    raw <- jsonlite::fromJSON(content)
+
+  }
+
+  endpoint <- extract_endpoint(resp$url)
+
+  # return congress_api S3 object
+  out <- structure(
+    list(
+      content = raw,
+      path = resp$url,
+      response = resp
+    ),
+    class = c("congress_api", endpoint)
+  )
+
+  return(out)
+}
+
+# S3 print method for congress_api class objects
+print.congress_api <- function(x, ...) {
+
+  cat("<Congress API: ", x$path, ">\n", sep = "")
+  utils::str(x$content)
+  invisible(x)
+
+}
+
+# based on https://github.com/hrecht/censusapi/blob/HEAD/R/getcensus_functions.R
+check_response <- function(resp) {
+
+  content <-
+    httr::content(resp, as="text",
+                  encoding = "UTF-8")
+
+  if (resp$status_code != 200) {
+
+    err_msg <-
+      trimws(
+        gsub("[\\{\\}\n\"]|(e|E)rror|:", "", content)
+      )
+
+    stop(
+      paste0(err_msg,"\nYour API call was: ",
+             resp$url),
+      call. = FALSE)
+
+  }
+
+  if (identical(content, "")) {
+    stop("No content was returned.\nYour API call was: ",
+         resp$url,
+         call. = FALSE)
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 congressGet <-
   function(endpoint,
