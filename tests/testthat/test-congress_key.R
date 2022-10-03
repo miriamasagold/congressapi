701345703fe5f1b6f016c9002ab6f67385907b6f
miriamgold <goldasa99@gmail.com>
2022-10-03 10:19:56 -0500

    add set_key user prmpt test

--- tests/testthat/test-congress_key.R
+++ tests/testthat/test-congress_key.R
@@ -2,7 +2,7 @@
 # congress_get_key() tests
 #==========================================
 
-test_that("congress_set_key() works", {
+test_that("congress_get_key() works", {
 
   old_key <- congress_get_key()
   on.exit(congress_set_key(old_key),
@@ -12,6 +12,19 @@ test_that("congress_set_key() works", {
   invisible(congress_set_key("new_key"))
 
   expect_identical(congress_get_key(), "new_key")
+
+})
+
+test_that("congress_set_key() prompts user input in console", {
+
+  old_key <- congress_get_key()
+  on.exit(congress_set_key(old_key),
+          add = TRUE,
+          after = FALSE)
+
+  invisible(congress_set_key("new_key"))
+
+  expect_output(congress_set_key("newest_key", warn = FALSE))
 
 })
 
